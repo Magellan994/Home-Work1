d0a268c079ae80e9c60de9a6315abb54844904a9
Magellan994 <khukha994@gmail.com>
2022-10-17 15:34:25 +0300

    changes 17_10_22 newq

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
-const name = prompt ('Your Name?');
-alert (`Hello, ${name}! How are you?.`);
-const isUser = confirm(`Where is ${name} ?.`);
-if(isUser){
-    alert('Nice to meet You')
-}
+//1
+
+//const a = prompt("число а")
+//const b = prompt("число b")
+//alert(Number(a)+Number(b));
+
+//2
+
+//const name = prompt("What is your name");
+//alert(`Hello ${name.charAt(0).toUpperCase() +name.slice(1)}`);
+
+//3
+
+//const question = prompt('Is it You?');
+//if(question){
+//    alert('true - ' +question);
+//} else {
+ //   alert( 'false -' + question)
+//}
+
+//4
+
+//const userText = prompt(
+   // "Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
+//if (userText){
+  // console.log(userText.slice(0, 5))}
+ //  {console.log(userText.slice(52))}
+
 
 
