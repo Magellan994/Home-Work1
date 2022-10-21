0b15ddab72766dbac3b4bccc918339ce5ae63208
Magellan994 <khukha994@gmail.com>
2022-10-21 19:06:51 +0300

    changes 21_10_22 newq

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
-//1
 
-//const a = prompt("число а")
-//const b = prompt("число b")
-//alert(Number(a)+Number(b));
+ const str = prompt('Is it You?');
+let lowStr = str.toLowerCase()
+ if (lowStr.indexOf('free')!=-1) {
+       alert(true)
+      console.log(true)
+ } else {
+       alert(false)
+        console.log(false)
+}
 
-//2
-
-//const name = prompt("What is your name");
-//alert(`Hello ${name.charAt(0).toUpperCase() +name.slice(1)}`);
-
-//3
-
-//const question = prompt('Is it You?');
-//if(question){
-//    alert('true - ' +question);
-//} else {
- //   alert( 'false -' + question)
-//}
-
-//4
-
-//const userText = prompt(
-   // "Lorem ipsum dolor sit amet, consectetur adipisicing elit.")
-//if (userText){
-  // console.log(userText.slice(0, 5))}
- //  {console.log(userText.slice(52))}
 
 
 
