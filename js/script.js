504dc0b619274220cd0faa33a8b216d671d55811
Magellan994 <khukha994@gmail.com>
2022-10-25 12:51:53 +0300

    changes 24_10_22 newq

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 
- const str = prompt('Is it You?');
-let lowStr = str.toLowerCase()
- if (lowStr.indexOf('free')!=-1) {
-       alert(true)
-      console.log(true)
- } else {
-       alert(false)
-        console.log(false)
-}
+ //задание 1
+// let result;
+ //result = ( a + b < 4) ? 'Много': 'Мало';
+
+// задание 2
+//const number =prompt('?')
+ //if (number>0) {
+ // alert(1);
+// } else if (number <0){
+ // alert(-1)
+ //}else {
+ // alert(0)
+// }
+
+// задание 3
+
+// const age =prompt('Your age?',14);
+//if ( age > 14 , age <90 ){
+// alert('Wellcom');}
+
+
+
+
 
 
 
